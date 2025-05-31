ccff16c2be3c7654082f0383d193263b8024f7dc
faraz ali <faraz127200@outlook.com>
2025-05-31 09:03:11 +0500

    project with if

---
+++ project1/chaiaurcode.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if(e.target.id === 'grey')
+        {
+            body.style.backgroundColor = 'grey' // or e.target.id
+        }
+        if(e.target.id === 'white')
+        {
+            body.style.backgroundColor = 'white' // or e.target.id
+        }
+        if(e.target.id === 'blue')
+        {
+            body.style.backgroundColor = 'blue' // or e.target.id
+        }
+        if(e.target.id === 'yellow')
+        {
+            body.style.backgroundColor = 'yellow' // or e.target.id
+        }
+        if(e.target.id === 'purple')
+        {
+            body.style.backgroundColor = 'purple' // or e.target.id
+        }
+        if(e.target.id === 'red')
+        {
+            body.style.backgroundColor = 'red' // or e.target.id
+        }
+    })
+    
+})
